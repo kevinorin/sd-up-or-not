fc667e8d95a4b50e755bca8066f22b184875718c
TA <ta@bgn.games>
2022-09-04 13:18:21 -0400

    install and initialize apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react";
-import Header from '../components/Header'
+import { ApolloProvider } from '@apollo/client';
+import Header from '../components/Header';
+import client from '../apollo-client.js';
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <div className='h-screen overflow-y-scroll bg-slate-300'>
-        <Header />
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <div className='h-screen overflow-y-scroll bg-slate-300'>
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </ApolloProvider>
   )
 }
 
